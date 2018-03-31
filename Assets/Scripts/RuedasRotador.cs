1af29388da66eeb0b19eb88edc2f7d4d79cc6690
Cristian Pardo (MAC) <pardocrespocristian@gmail.com>
2018-03-31 12:37:44 -0500

    rotar ruedas con axis

--- Assets/Scripts/RuedasRotador.cs
+++ Assets/Scripts/RuedasRotador.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityStandardAssets.CrossPlatformInput;
 public class RuedasRotador : MonoBehaviour {
 
     public float rotacionSpeed = 10.0f;
     private void FixedUpdate()
     {
         
-        if (Input.GetAxis("Horizontal") > 0) {
+		if (CrossPlatformInputManager.GetAxis("Horizontal") > 0) {
             transform.Rotate(Vector3.forward * -rotacionSpeed);
-        }else if  (Input.GetAxis("Horizontal") < 0)
+		}else if  (CrossPlatformInputManager.GetAxis("Horizontal") < 0)
         {
                 transform.Rotate(Vector3.forward * rotacionSpeed);
 
